f84bf699524d937bc1f4d4aa2ae5cdab83fc722b
HahaBudda <goodyamaki@mail.ru>
2022-02-28 18:15:00 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignIn from "./Companents/SingIn/SignIn";
+import Createacc from "./Companents/Createacc/Createacc";
 
 function App() {
   return (
     <div className="App">
     <SignIn/>
+    {/*  <Createacc/>*/}
     </div>
   );
 }
--- src/Companents/Createacc/Createacc.jsx
+++ src/Companents/Createacc/Createacc.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 
-import styles from './SignIn.module.scss'
-
-import PreviewImg from '../../Assets/preview.jpg'
-
+import styles from './Createacc.module.scss'
 import Input from "../Input/Input";
 import Button from "../Button/Button";
+import PreviewImg from "../../Assets/preview.jpg";
 
-const SignIn = () => {
+const Createacc = () => {
 	return (
 		<main className={styles.main}>
 			<div className={styles.main__page}>
-				<h1 className={styles.main__title}>Sign in</h1>
+				<h1 className={styles.main__title}>Create an account</h1>
 				<div className={styles.main__signin}>
 					<form className={styles.main__signin__form}>
-						<label className={styles.main__signin__label}>
-						Email
-						<Input/>
+						<div className={styles.main__signin__label__wrapper}><label className={styles.main__signin__label}>
+							Email
+							<Input/>
 						</label>
 						<label className={styles.main__signin__label}>
-						Password
-						<Input/>
+							Email
+							<Input/>
+						</label>
+						</div>
+
+						<label className={styles.main__signin__label}>
+							Email
+							<Input/>
+						</label>
+						<label className={styles.main__signin__label}>
+							Password
+							<Input/>
+						</label>
+						<label className={styles.main__signin__label}>
+							Email
+							<Input/>
+						</label>
+						<label className={styles.main__signin__label}>
+							Password
+							<Input/>
 						</label>
 						<label className={styles.main__signin__button}><Button/></label>
 					</form>
@@ -30,9 +46,11 @@ const SignIn = () => {
 			<div className={styles.main__preview}>
 				<img className={styles.main__preview__img} alt={"PreviewImg"} src={PreviewImg}/>
 			</div>
-			
+
 		</main>
+
+
 	);
 };
 
-export default SignIn;
+export default Createacc;
